2a78a81adc0b09208e89a1e5d581fc423febef3d
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2025-12-07 23:19:28 -0300

    feat: INTRODUÇÃO OK

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const modulo = 'Módulo 2'
+  const nomeDoAluno = '<span style="color: red">Lucas</span>'
+  const btnDisabled = false
+
+  const atributos = {
+    style: 'color: blue',
+    id: 'titulo-principal',
+    class: 'titulo-color'
+  }
+</script>
+
+<template>
+  <h1 :v-bind="atributos" :title="modulo">Curso vuejs expert - {{ modulo }}</h1>
+  <p v-html="nomeDoAluno"></p>
+  <button :disabled="btnDisabled">Salvar</button>
+</template>
+
+<style scoped>
+
+</style>
