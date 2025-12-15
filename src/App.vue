f9ee4a7add8b713b9c7c455a4ee5ad750c74fc2c
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2025-12-14 21:47:00 -0300

    feat: Aula 7 M1 - Referências de modelo(ref)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <script setup>
-import { ref , watch, watchEffect } from 'vue'
+import { ref, onMounted } from 'vue'
 
-const inputName = ref('')
-const usuario = ref('')
-const data = ref(null)
+const input = ref(null)
 
-function setName() {
-  usuario.value = inputName.value
-}
-
-watch(usuario, (newValue, oldValue) => {
-  console.log('Valor antigo', oldValue)
-  console.log('Valor novo', newValue)
+onMounted(() => {
+  console.log(input.value.focus())
 })
 
-watchEffect(async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${usuario.value}`)
-
-  data.value = await response.json()
-  console.log(response)
-})
 </script>
 
 <template>
   <div>
-    <input type="text" v-model="inputName">
-    <button @click="setName">Enviar nome</button>
+    <input type="text" ref="input">
   </div>
 </template>
